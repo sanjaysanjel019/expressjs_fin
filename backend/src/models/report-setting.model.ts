9b4d8c406597b975c323d4d4b00a137cc3fb816f
sanjaysanjel019 <sanjay.sanjel018@gmail.com>
2025-07-26 22:25:13 +0545

    added report and report settings model

---
+++ backend/src/models/report-setting.model.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+export enum ReportFrequencyEnum {
+  MONTHLY = "MONTHLY",
+}
+
+export interface ReportSettingDocument extends Document {
+  userId: mongoose.Types.ObjectId;
+  frequency: keyof typeof ReportFrequencyEnum;
+  isEnabled: boolean;
+  nextReportDate?: Date;
+  lastSentDate?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const reportSettingSchema = new mongoose.Schema<ReportSettingDocument>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    frequency: {
+      type: String,
+      required: true,
+    },
+    isEnabled: {
+      type: Boolean,
+      default: false,
+    },
+    nextReportDate: {
+      type: Date,
+    },
+    lastSentDate: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const ReportSettingModel = mongoose.model<ReportSettingDocument>(
+  "ReportSetting",
+  reportSettingSchema
+);
+
---
+++ backend/src/models/report.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+export enum ReportStatusEnum {
+    SENT="SENT",
+    PENDING="PENDING",
+    FAILED="FAILED",
+}
+
+export interface ReportDocument extends Document {
+    userId:mongoose.Types.ObjectId;
+    period:string;
+    sentDate:Date;
+    status:keyof typeof ReportStatusEnum;
+    createdAt:Date;
+    updatedAt:Date;   
+}
+
+const reportSchema = new mongoose.Schema<ReportDocument>({
+    userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
+    period:{type:String,required:true},
+    sentDate:{type:Date,required:true},
+    status:{
+        type:String,
+        enum:Object.values(ReportStatusEnum),
+        default:ReportStatusEnum.PENDING,
+        required:true,
+    }
+});
+
+const ReportModel = mongoose.model<ReportDocument>("Report",reportSchema);
+export default ReportModel;
